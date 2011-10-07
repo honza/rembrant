7af6309bdb5d19721762202c34475bd014519d2d
Honza Pokorny <me@honza.ca>
2011-10-06 21:43:25 -0300

    Delete everything.

--- rembrant/gui/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from models import *
-
-
-admin.site.register(Tag)
-admin.site.register(Set)
-admin.site.register(Person)
-admin.site.register(Place)
-admin.site.register(Photo)
